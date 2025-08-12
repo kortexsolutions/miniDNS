26082d8fcd0fdfe56b2f7c45615040bb904f42e1
Marvin Mullings <marvin.mullings@jca.gov.jm>
2025-08-12 16:22:01 -0600

    Created project boiler plate structure for mini DNS project

---
+++ src/main/java/io/speer/miniDNS/MiniDnsApplication.java
@@ -0,0 +1,13 @@
+package io.speer.miniDNS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniDnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/speer/miniDNS/MiniDnsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.speer.miniDNS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
