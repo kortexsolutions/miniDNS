4c2c8e3696f7271785c716af86811cb8a67717c0
Marvin Mullings <marvin.mullings@jca.gov.jm>
2025-08-13 23:36:35 -0600

    feature/retrieve-dns-record - Address api resource for retrieving dns records

--- src/main/java/io/speer/miniDNS/controller/HostController.java
+++ src/main/java/io/speer/miniDNS/controller/HostController.java
@@ -1,7 +1,9 @@
 package io.speer.miniDNS.controller;
 
-import io.speer.miniDNS.common.enums.TypeEnum;
+import io.speer.miniDNS.common.Message;
+import io.speer.miniDNS.dto.HostListResponseDto;
 import io.speer.miniDNS.dto.HostRequestDto;
+import io.speer.miniDNS.dto.HostResolveResponseDto;
 import io.speer.miniDNS.dto.HostResponseDto;
 
 import io.speer.miniDNS.service.HostService;
@@ -25,36 +27,26 @@ public class HostController {
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public ResponseEntity<HostResponseDto> addDNSRecord(@RequestBody HostRequestDto host) throws Exception {
         if (!whiteList.contains(host.getType().toLowerCase()))
-            throw new BadRequestException("This Host type is not supported!");
+            throw new BadRequestException(Message.NOT_ALLOWED);
 
         /* save dns record base on host type */
         HostResponseDto response = _hService.save(host);
-
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-
     @RequestMapping(value = "/{hostname}", method = RequestMethod.GET)
-    public ResponseEntity<?> resolveHostname(@PathVariable("hostname") String hostname) throws Exception {
+    public ResponseEntity<HostResolveResponseDto> resolveHostname(@PathVariable("hostname") String hostname) throws Exception {
+        if (hostname == null || hostname.isEmpty())
+            throw new BadRequestException(Message.NULL_VALUE);
 
-
-        return null;
+        return new ResponseEntity<>(_hService.resolve(hostname), HttpStatus.OK);
     }
-
-
 
     @RequestMapping(value = "/{hostname}/records", method = RequestMethod.GET)
-    public ResponseEntity<?> resolveDNSRecords(@PathVariable("hostname") String hostname) throws Exception {
+    public ResponseEntity<HostListResponseDto> listDNSRecords(@PathVariable("hostname") String hostname) throws Exception {
+        if (hostname == null || hostname.isEmpty())
+            throw new BadRequestException(Message.NULL_VALUE);
 
-        return null;
+        return new ResponseEntity<>(_hService.records(hostname), HttpStatus.OK);
     }
-
-    @RequestMapping(value = "/{hostname}/?", method = RequestMethod.DELETE)
-    public void deleteDNSRecord(@PathVariable("hostname") String hostname,
-                                @RequestParam String type,
-                                @RequestParam String value) throws Exception {
-
-    }
-
-
 }
