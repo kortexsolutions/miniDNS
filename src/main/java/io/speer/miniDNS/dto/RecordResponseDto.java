af5c221e4c08e4cf9cf766a9183e8ae22e2a7f24
Marvin Mullings <marvin.mullings@jca.gov.jm>
2025-08-13 23:34:37 -0600

    feature/retrieve-dns-record - Added several dto for data response purposes

--- src/main/java/io/speer/miniDNS/dto/RecordResponseDto.java
+++ src/main/java/io/speer/miniDNS/dto/RecordResponseDto.java
@@ -5,16 +5,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import java.time.LocalDateTime;
-
 @Data
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-public class HostResponseDto {
-    private String hostName;
+public class RecordResponseDto {
     private String type;
     private String value;
-    private LocalDateTime createdAt;
 }
 
