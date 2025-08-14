d1a39dd6ed93a760460bfbb75522f6e6d5deff09
Marvin Mullings <marvin.mullings@jca.gov.jm>
2025-08-13 23:36:55 -0600

    feature/retrieve-dns-record - Added display request messages

---
+++ src/main/java/io/speer/miniDNS/common/Message.java
@@ -0,0 +1,11 @@
+package io.speer.miniDNS.common;
+
+public class Message {
+
+    public static final String NOT_ALLOWED = "This host type is not supported!";
+    public static final String NULL_VALUE = "Oh no! You have not entered a value.";
+    public static final String NOT_FOUND = "Sorry! No data found matching your request.";
+    public static final String EXIST_CNAME = "Unable to add CName record with existing record entry.";
+    public static final String EXIST_RECORD = "Unable to add a record with existing CName entry.";
+
+}
