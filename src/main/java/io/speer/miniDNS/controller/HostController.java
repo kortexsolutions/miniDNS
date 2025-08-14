d04af3b32c8cdb34d0b23e6c2334706df6947079
Marvin Mullings <marvin.mullings@jca.gov.jm>
2025-08-14 17:12:27 -0600

    feature/remove-dns-record - Included delete resource endpoint

--- src/main/java/io/speer/miniDNS/controller/HostController.java
+++ src/main/java/io/speer/miniDNS/controller/HostController.java
@@ -5,13 +5,13 @@ import io.speer.miniDNS.dto.HostListResponseDto;
 import io.speer.miniDNS.dto.HostRequestDto;
 import io.speer.miniDNS.dto.HostResolveResponseDto;
 import io.speer.miniDNS.dto.HostResponseDto;
-
 import io.speer.miniDNS.service.HostService;
-import org.apache.coyote.BadRequestException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,32 +21,61 @@ import java.util.List;
 public class HostController {
     @Autowired
     private HostService _hService;
-
     private List<String> whiteList = Arrays.asList("a", "cname");
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
-    public ResponseEntity<HostResponseDto> addDNSRecord(@RequestBody HostRequestDto host) throws Exception {
+    public ResponseEntity<HostResponseDto> addDNSRecord(@RequestBody HostRequestDto host) {
         if (!whiteList.contains(host.getType().toLowerCase()))
-            throw new BadRequestException(Message.NOT_ALLOWED);
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, Message.NOT_ALLOWED);
 
-        /* save dns record base on host type */
-        HostResponseDto response = _hService.save(host);
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        try {
+            /* save dns record base on host type */
+            HostResponseDto response = _hService.save(host);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, ex.getMessage());
+        }
     }
 
     @RequestMapping(value = "/{hostname}", method = RequestMethod.GET)
-    public ResponseEntity<HostResolveResponseDto> resolveHostname(@PathVariable("hostname") String hostname) throws Exception {
+    public ResponseEntity<HostResolveResponseDto> resolveHostname(@PathVariable("hostname") String hostname) {
         if (hostname == null || hostname.isEmpty())
-            throw new BadRequestException(Message.NULL_VALUE);
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, Message.NULL_VALUE);
 
-        return new ResponseEntity<>(_hService.resolve(hostname), HttpStatus.OK);
+        try {
+            return new ResponseEntity<>(_hService.resolve(hostname), HttpStatus.OK);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, ex.getMessage());
+        }
     }
 
     @RequestMapping(value = "/{hostname}/records", method = RequestMethod.GET)
-    public ResponseEntity<HostListResponseDto> listDNSRecords(@PathVariable("hostname") String hostname) throws Exception {
+    public ResponseEntity<HostListResponseDto> listDNSRecords(@PathVariable("hostname") String hostname) {
         if (hostname == null || hostname.isEmpty())
-            throw new BadRequestException(Message.NULL_VALUE);
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, Message.NULL_VALUE);
 
-        return new ResponseEntity<>(_hService.records(hostname), HttpStatus.OK);
+        try {
+            return new ResponseEntity<>(_hService.records(hostname), HttpStatus.OK);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, ex.getMessage());
+        }
+    }
+
+    @RequestMapping(value = "/{hostname}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> removeDSRecord(@PathVariable("hostname") String hostname,
+                                            @RequestParam String type,
+                                            @RequestParam String value) {
+        try {
+            if (hostname == null || type == null || value == null)
+                throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, Message.NULL_VALUE);
+
+            return new ResponseEntity<>(_hService.remove(hostname, type, value), HttpStatus.OK);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, ex.getMessage());
+        }
     }
 }
